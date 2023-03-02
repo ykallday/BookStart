1f3a08effa9b0c258ec05e96eb0cb81b035376d2
Yael Kaufman <ylkauf@gmail.com>
2023-03-02 15:17:31 -0800

    final updates

--- bookstart-app/src/components/Nav.jsx
+++ bookstart-app/src/components/Nav.jsx
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom'
 export default function Nav(){
     return(
         <div className = "Main-Nav">
-            <Link to ="/">Home</Link>
-            <Link to ="/SearchBar">Search</Link>
-            <Link to ="/Browse">Browse</Link>
-            <Link to ="/Wishlist">Wishlist</Link>
+            <Link id="Home" to ="/">Home</Link>
+            <Link id="Search" to ="/SearchBar">Search</Link>
+            <Link id="Browse" to ="/Browse">Browse</Link>
+            <Link id="Wish" to ="/Wishlist">Wishlist</Link>
         </div>
     )
 }
